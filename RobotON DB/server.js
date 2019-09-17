dedc69de56d40e8b1743be8c9f559e8cd66153f9
Ibrahimmushtaq98 <ibrahim.mushtaq@ontariotechu.net>
2019-09-17 19:17:46 -0400

    Updated the DB with more docker stuff

--- RobotON DB/server.js
+++ RobotON DB/server.js
@@ -2,25 +2,32 @@
 // The only security in this, is that noone is touch the DB, everything has to go through a API Call
 var express = require('express'),
 app = express(),
-port = 3000,
 mongoose = require('mongoose'),
 Task = require('./api/models/model'),
 bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000;
+
 //Make use of CORS
 var cors = require('cors');
-
 app.use(cors());
+
+const {
+  MONGO_DB,
+  DATABASE_URL,
+  DATABASE_PORT
+} = process.env;
+
+const url = `mongodb://${DATABASE_URL}:${DATABASE_PORT}/${MONGO_DB}`;
+console.log(url);
 
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 1000 // Reconnect every 500ms
+    reconnectInterval: 1000 // Reconnect every 1000ms
 }
-
-var url = "mongodb://mongo:27017/robo";
 
 //Intialize mongo
 mongoose.Promise = global.Promise;
@@ -65,7 +72,7 @@ var routes = require('./api/routes/routes');
 routes(app);
 
 
-app.listen(port);
-
-console.log("Server started on port " + port);
+app.listen(port, function(){
+  console.log(`Server started on port ${port}`);
+});
 
