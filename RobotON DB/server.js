98e1d830bb8a479a92fcbdb00ef6c9a956183345
Mario <mario.velazquez1@ontariotechu.net>
2022-10-27 11:05:06 -0400

    Added CORS

--- RobotON DB/server.js
+++ RobotON DB/server.js
@@ -1,67 +1,25 @@
 //Server for the API calls for DB
-// The only security in this, is that no one is touch the DB, everything has to go through a API Call
+// The only security in this, is that noone is touch the DB, everything has to go through a API Call
 var express = require('express'),
 app = express(),
+port = 3000,
 mongoose = require('mongoose'),
 Task = require('./api/models/model'),
 bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
-
 //Make use of CORS
 var cors = require('cors');
+
 app.use(cors());
 
-const {
-  MONGO_DB,
-  DATABASE_URL,
-  DATABASE_PORT
-} = process.env
-const url = `mongodb://${DATABASE_URL}:${DATABASE_PORT}/${MONGO_DB}`;
-console.log(url);
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 1000 // Reconnect every 1000ms
-}
+var url = "mongodb://localhost:27017/robo";
 
 //Intialize mongo
 mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-
-db.on('connecting', function() {
-console.log('connecting to MongoDB...');
-});
-
-db.on('error', function(error) {
-console.error('Error in MongoDb connection: ' + error);
-mongoose.disconnect();
-});
-db.on('connected', function() {
-    console.log('MongoDB connected!');
-});
-db.once('open', function() {
-    console.log('MongoDB connection opened!');
-});
-db.on('reconnected', function () {
-    console.log('MongoDB reconnected!');
-});
-db.on('disconnected', function() {
-    console.log('MongoDB disconnected!');
-});
-
-var connectWithRetry = function() {
-    return mongoose.connect(url,options, function(err) {
-      if (err) {
-        console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
-        setTimeout(connectWithRetry, 5000);
-      }
-    });
-  };
-connectWithRetry();
+mongoose.connect(url, { useNewUrlParser: true });
+//Deprecated Option
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -71,7 +29,50 @@ var routes = require('./api/routes/routes');
 routes(app);
 
 
-app.listen(port, function(){
-  console.log(`Server started on port ${port}`);
-});
+app.listen(port);
 
+console.log("Server started on port " + port);
+
+//Third Party Library, may not be best options tbh
+var XMLHttpRequest = require("xhr2").XMLHttpRequest;
+
+//Here you can set how often the MLData is updated
+updateMLData();
+getMLData();
+setInterval(updateMLData, 6000);
+runServerML();
+
+//This is the function that calls the server MLDataUpdate function
+function updateMLData() {
+    console.log("Machine Learning Data Updated");
+    var request = new XMLHttpRequest();
+
+    //This Line needs to be changed when we go to Production
+    request.open('GET', "http://localhost:3000/logs/tryingJS")
+    request.send();
+}
+
+//This function won't be used, here for debugging
+function getMLData(){
+    console.log("Machine Learning Data Gotten");
+    var request = new XMLHttpRequest();
+
+    //This Line needs to be changed when we go to Production
+    request.open('GET', "http://localhost:3000/logs/ml")
+    request.send();
+    request.onload = ()=>{
+        //console.log(JSON.parse(request.response));
+    }
+}
+
+function runServerML(){
+    console.log("Machine Learning model being updated");
+    var request = new XMLHttpRequest();
+
+    //This Line needs to be changed when we go to Production
+    request.open('GET', "http://localhost:3000/ml/level1a.xml/50000/0")
+    request.send();
+    request.onload = ()=>{
+        console.log("Machine Learning done!");
+    }
+}
